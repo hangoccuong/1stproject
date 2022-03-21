b9b5833e0bd7092baa0c5a2d2e59a37d39c180a1
hangoccuong <101912693+hangoccuong@users.noreply.github.com>
2022-03-21 22:47:14 +0700

    Create ve_co_nhat_BTVN.py

---
+++ ve_co_nhat_BTVN.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import turtle
+tl=turtle.Turtle()
+tl.shape('turtle')
+tl.pensize(1)
+tl.pencolor('black')
+tl.speed(1)
+tl.fillcolor('white')
+tl.begin_fill()
+tl.forward(150)
+tl.right(90)
+tl.backward(200)
+tl.right(90)
+tl.forward(300)
+tl.left(90)
+tl.forward(200)
+tl.left(90)
+tl.forward(210)
+tl.penup()
+tl.left(90)
+tl.forward(100)
+tl.fillcolor('#bc002d')
+tl.begin_fill()
+tl.circle(60)
+tl.end_fill()
+tl.penup()
+tl.right(20)
+tl.end_fill()
+turtle.done()
+
+
+# In[ ]:
+
+
+
+
